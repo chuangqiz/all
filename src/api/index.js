b2969379ee77dd8170030698131eee91fa5adfa8
zhangchuangqi <1075688050@qq.com>
2021-03-11 19:06:27 +0800

    第一次添加

--- src/api/index.js
+++ src/api/index.js
@@ -4,16 +4,18 @@ const instance = axios.create({
   timeout: 10000
 })
 
-function $get(url, data = {}) {
-  return instance.get(url, { params: data })
-}
+// function $get(url, data = {}) {
+//   return instance.get(url, { params: data })
+// }
 
-function $post(url, data = {}) {
+function $post (url, data = {}) {
   return instance.post(url, data)
 }
 
 class Services {
   users = {
-    
+    login: data => $post('/v2', data)
   }
 }
+
+export const services = new Services()
